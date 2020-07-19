9127bc906cdbc245592bcb9ef9440c7e73447269
Radi Obad <lovelovelove67@gmail.com>
2020-07-19 17:10:04 +0300

    Adding ordering to module and content objects

--- courses/models.py
+++ courses/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
+from .fields import OrderField
 
 class Subject(models.Model):
     title = models.CharField(max_length=200)
@@ -33,27 +34,37 @@ class Course(models.Model):
         return self.title
 
 class Module(models.Model):
-	course = models.ForeignKey(Course,
-	                           related_name='modules',
-	                           on_delete=models.CASCADE)
-	title = models.CharField(max_length=200)
-	description = models.TextField(blank=True)
-	def __str__(self):
-		return self.title
+    course = models.ForeignKey(Course,
+                               related_name='modules',
+                               on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    order = OrderField(blank=True, for_fields=['course'])
+
+    def __str__(self):
+        return f'{self.order}. {self.title}'
+
+    class Meta:
+        ordering = ['order']
+
 
 class Content(models.Model):
-	module = models.ForeignKey(Module,
-	                           related_name='contents',
-	                           on_delete=models.CASCADE)
-	content_type = models.ForeignKey(ContentType,
-	                                 on_delete=models.CASCADE,
-	                                 limit_choices_to={'model__in':(
-	                                 'text',
-	                                 'video',
-	                                 'image',
-	                                 'file')})
-	object_id = models.PositiveIntegerField()
-	item = GenericForeignKey('content_type', 'object_id')
+    module = models.ForeignKey(Module,
+                               related_name='contents',
+                               on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType,
+                                     on_delete=models.CASCADE,
+                                     limit_choices_to={'model__in':(
+                                     'text',
+                                     'video',
+                                     'image',
+                                     'file')})
+    object_id = models.PositiveIntegerField()
+    item = GenericForeignKey('content_type', 'object_id')
+    order = OrderField(blank=True, for_fields=['module'])
+
+    class Meta:
+        ordering = ['order']
 
 
 class ItemBase(models.Model):
